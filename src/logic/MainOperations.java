264831c583954516246b6876c4f131fbb37cb433
Roman Nechaev <70903393+RomanNechaev@users.noreply.github.com>
2022-03-01 20:43:21 +0500

    добавил классы для работы с логикой

---
+++ src/logic/MainOperations.java
@@ -0,0 +1,11 @@
+package logic;
+
+public class MainOperations {
+    public static double applyLimits(double value, double min, double max) {
+        if (value < min)
+            return min;
+        if (value > max)
+            return max;
+        return value;
+    }
+}
---
+++ src/logic/MoveOperations.java
@@ -0,0 +1,10 @@
+package logic;
+
+import static logic.MainOperations.applyLimits;
+import static logic.MathOperations.asNormalizedRadians;
+import static logic.RobotConstans.*;
+import static logic.RobotConstans.m_robotDirection;
+
+public class MoveOperations {
+
+}
