9649d66b4bc02f9b8ac4ace1978231239009d975
Roman Nechaev <70903393+RomanNechaev@users.noreply.github.com>
2022-03-13 21:21:18 +0500

    добавил метод для десериализации

--- src/gui/WindowsCommon.java
+++ src/gui/WindowsCommon.java
@@ -8,25 +8,31 @@ import javax.swing.event.InternalFrameEvent;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.beans.PropertyVetoException;
+import java.io.*;
 import java.util.ArrayList;
 
 public abstract class WindowsCommon {
     static void exitWindow(Component window) {
-        if (window instanceof JInternalFrame)
-        {
-            ((JInternalFrame) window).addInternalFrameListener(new InternalFrameAdapter(){
+        if (window instanceof JInternalFrame) {
+            ((JInternalFrame) window).addInternalFrameListener(new InternalFrameAdapter() {
                 public void internalFrameClosing(InternalFrameEvent e) {
                     if (confirmClosing(e.getInternalFrame())) {
                         e.getInternalFrame().getDesktopPane().getDesktopManager().closeFrame(e.getInternalFrame());
+                        var windowLocation = e.getInternalFrame().getLocation();
+                        var windowLocationConfig = new Configuration(windowLocation.x, windowLocation.y);
+                        writeConfig(windowLocationConfig);
                     }
                 }
             });
-        }
-        else {
+        } else {
             ((JFrame) window).addWindowListener(new WindowAdapter() {
                 public void windowClosing(WindowEvent e) {
                     if (confirmClosing(e.getWindow())) {
                         e.getWindow().setVisible(false);
+                        var windowLocation = e.getWindow().getLocation();
+                        var windowLocationConfig = new Configuration(windowLocation.x, windowLocation.y);
+                        writeConfig(windowLocationConfig);
                         System.exit(0);
                     }
                 }
@@ -34,8 +40,44 @@ public abstract class WindowsCommon {
         }
     }
 
-    static boolean confirmClosing(Component window)
-    {
+    public static void writeConfig(Configuration config) {
+        File file = new File("data.bin");
+        try {
+            OutputStream os = new FileOutputStream(file);
+            try {
+                ObjectOutputStream oos =
+                        new ObjectOutputStream(new BufferedOutputStream(os));
+                try {
+                    oos.writeObject(config);
+                    oos.flush();
+                } finally {
+                    oos.close();
+                }
+            } finally {
+                os.close();
+            }
+            InputStream is = new FileInputStream(file);
+            try {
+                ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(is));
+                try {
+                    Configuration restored = (Configuration) ois.readObject();
+                    System.out.println(restored.getGameWindowLocationX() + ": " + restored.getGameWindowLocationY());
+                    boolean bSame = (config == restored);
+                    System.out.println("Same object: " + bSame);
+                } catch (ClassNotFoundException ex) {
+                    ex.printStackTrace();
+                } finally {
+                    ois.close();
+                }
+            } finally {
+                is.close();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    static boolean confirmClosing(Component window) {
         String[] options = {"закрыть", "отмена"};
         int answer = JOptionPane.showOptionDialog(window,
                 "Вы уверены?",
@@ -46,8 +88,7 @@ public abstract class WindowsCommon {
         return answer == 0;
     }
 
-    public static boolean confirmClearing()
-    {
+    public static boolean confirmClearing() {
         String[] options = {"да", "нет"};
         int answer = JOptionPane.showOptionDialog(null,
                 "Лог заполнен. Добавить сообщение?",
