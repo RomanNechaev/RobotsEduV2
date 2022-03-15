7f3a0af053bd5994690646e329cf84a20fe22080
Roman Nechaev <70903393+RomanNechaev@users.noreply.github.com>
2022-03-15 21:24:31 +0500

    добавил метод для десериализации очереди из GameWindow

--- src/gui/MainApplicationFrame.java
+++ src/gui/MainApplicationFrame.java
@@ -5,8 +5,13 @@ import java.beans.PropertyVetoException;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayDeque;
 import javax.swing.*;
+
 import log.Logger;
+
 import static gui.WindowsCommon.*;
 import static gui.WindowsConst.*;
 
@@ -14,11 +19,10 @@ import static gui.WindowsConst.*;
  * Что требуется сделать:
  * 1. Метод создания меню перегружен функционалом и трудно читается.
  * Следует разделить его на серию более простых методов (или вообще выделить отдельный класс).
- *
  */
-public class MainApplicationFrame extends JFrame
-{
+public class MainApplicationFrame extends JFrame {
     private final JDesktopPane desktopPane = new JDesktopPane();
+
     public MainApplicationFrame() throws IOException, ClassNotFoundException {
 
         //Make the big window be indented 50 pixels from each edge
@@ -26,55 +30,25 @@ public class MainApplicationFrame extends JFrame
         int inset = 50;
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         setBounds(inset, inset,
-            screenSize.width  - inset*2,
-            screenSize.height - inset*2);
+                screenSize.width - inset * 2,
+                screenSize.height - inset * 2);
 
         setContentPane(desktopPane);
         setJMenuBar(new gui.MenuGenerator());
+        setName("MainWindow");
+
 
 
         LogWindow logWindow = createLogWindow();
         GameWindow gameWindow = createGameWindow();
-//        try {
-//            var config = getConfig();
-//            gameWindow.setIcon(config.getIconState());
-//            gameWindow.setLocation(config.getLocationX(),config.getLocationY());
-//            gameWindow.setSize(config.getWindowWidth(),config.getWindowHeight());
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } catch (ClassNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (PropertyVetoException e) {
-//            e.printStackTrace();
-//        }
 
-        var t = 0;
-
-        while(t!=2) {
-            switch (getConfig().getName()) {
-                case "GameWindow":
-                    try {
-                        gameWindow.setIcon(getConfig().getIconState());
-                    } catch (PropertyVetoException e) {
-                        e.printStackTrace();
-                    }
-                    gameWindow.setLocation(getConfig().getLocationX(), getConfig().getLocationY());
-                    gameWindow.setSize(getConfig().getWindowWidth(), getConfig().getWindowHeight());
-                case "LogWindow":
-                    try {
-                        logWindow.setIcon(getConfig().getIconState());
-                    } catch (PropertyVetoException e) {
-                        e.printStackTrace();
-                    }
-                    logWindow.setLocation(getConfig().getLocationX(), getConfig().getLocationY());
-                    logWindow.setSize(getConfig().getWindowWidth(), getConfig().getWindowHeight());
-            }
-            t+=1;
-        }
+        if (Files.exists(Path.of(System.getProperty("user.home"), "data.bin")))
+            restoreConfiguration(logWindow, gameWindow);
 
 
         addWindow(logWindow);
         addWindow(gameWindow);
+
 
         exitWindow(gameWindow);
         exitWindow(logWindow);
@@ -83,30 +57,60 @@ public class MainApplicationFrame extends JFrame
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
     }
 
-    public static GameWindowConfiguration getConfig() throws IOException, ClassNotFoundException {
+    private void restoreConfiguration(LogWindow logWindow, GameWindow gameWindow) throws IOException, ClassNotFoundException {
+        var configs = getConfig();
+        while (!configs.isEmpty()) {
+            WindowConfiguration config = configs.pollLast();
+            switch (config.getName()) {
+                case "GameWindow" -> {
+                    try {
+                        gameWindow.setIcon(config.getIconState());
+                    } catch (PropertyVetoException e) {
+                        e.printStackTrace();
+                    }
+                    gameWindow.setLocation(config.getLocationX(), config.getLocationY());
+                    gameWindow.setSize(config.getWindowWidth(), config.getWindowHeight());
+                }
+                case "LogWindow" -> {
+                    try {
+                        logWindow.setIcon(config.getIconState());
+                    } catch (PropertyVetoException e) {
+                        e.printStackTrace();
+                    }
+                    logWindow.setLocation(config.getLocationX(), config.getLocationY());
+                    logWindow.setSize(config.getWindowWidth(), config.getWindowHeight());
+                }
+                case "MainWindow" -> {
+                    MainApplicationFrame.this.setState(config.getIconNumber());
+                    MainApplicationFrame.this.setLocation(config.getLocationX(), config.getLocationY());
+                    MainApplicationFrame.this.desktopPane.setSize(config.getWindowWidth(), config.getWindowHeight());
+                }
+            }
+        }
+    }
+
+    public static ArrayDeque<WindowConfiguration> getConfig() throws IOException, ClassNotFoundException {
         ObjectInputStream objectInputStream = null;
         try {
             objectInputStream = new ObjectInputStream(
-                    new FileInputStream(System.getProperty("user.home")+"\\"+"data.bin"));
+                    new FileInputStream(System.getProperty("user.home") + "\\" + "data.bin"));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        GameWindowConfiguration config = (GameWindowConfiguration) objectInputStream.readObject();
+        var configs = (ArrayDeque<WindowConfiguration>) objectInputStream.readObject();
         objectInputStream.close();
-        return config;
+        return configs;
     }
 
-    protected GameWindow createGameWindow()
-    {
+    protected GameWindow createGameWindow() {
         GameWindow gameWindow = new GameWindow();
         gameWindow.setSize(gameWindowWidth, gameWindowHeight);
         return gameWindow;
     }
 
-    protected LogWindow createLogWindow()
-    {
+    protected LogWindow createLogWindow() {
         LogWindow logWindow = new LogWindow(Logger.getDefaultLogSource());
-        logWindow.setLocation(10,10);
+        logWindow.setLocation(10, 10);
         logWindow.setSize(logWindowWidth, logWindowHeight);
         setMinimumSize(logWindow.getSize());
         logWindow.pack();
@@ -114,8 +118,7 @@ public class MainApplicationFrame extends JFrame
         return logWindow;
     }
 
-    protected void addWindow(JInternalFrame frame)
-    {
+    protected void addWindow(JInternalFrame frame) {
         desktopPane.add(frame);
         frame.setVisible(true);
     }
